26d5c870180dc03f34337ecbdaeb5421a6946f65
Shivashankar-0618 <shivashankarnerli@gmail.com>
2026-05-07 09:02:45 +0530

    fix: resolve merge conflicts gugjg

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") },
-  },
   build: {
     rollupOptions: {
-      external: ["fsevents"],
+      external: [],
     },
   },
+  optimizeDeps: {
+    exclude: ["fsevents"],
+  },
   server: {
-    port: 5174,
     proxy: {
       "/api": {
         target: "http://localhost:5000",
